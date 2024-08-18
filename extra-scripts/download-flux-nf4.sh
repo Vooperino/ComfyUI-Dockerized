7e1a73fb06a6afc3de1f07122ffe60b50ebd03c9
Voop <voop@voop.lv>
2024-08-18 17:42:33 +0300

    Adding Upscale models to be downloaded

--- extra-scripts/download-flux-nf4.sh
+++ extra-scripts/download-flux-nf4.sh
@@ -13,11 +13,26 @@ else
     echo "Custom Node 'ComfyUI_bitsandbytes_NF4' is installed!"
 fi
 
-echo "Checking if module exists"
+echo "Checking if checkpoint module exists"
 if [ ! -f "${ROOT}/checkpoints/flux1-dev-bnb-nf4-v2.safetensors" ]; then
     echo "File 'flux1-dev-bnb-nf4-v2.safetensors' was not found in the checkpoints. Downloading from 'https://huggingface.co/lllyasviel/flux1-dev-bnb-nf4' "
     cd "${ROOT}/checkpoints"
     wget https://huggingface.co/lllyasviel/flux1-dev-bnb-nf4/resolve/main/flux1-dev-bnb-nf4-v2.safetensors
+fi
+
+echo "Checking if upscale module exists"
+echo "Checking for '4x-UltraSharp.pth'"
+if [ ! -f "${ROOT}/upscale_models/4x-UltraSharp.pth" ]; then
+    echo "File '4x-UltraSharp.pth' was not found in the checkpoints. Downloading from 'https://huggingface.co/vclansience/SD_lora' "
+    cd "${ROOT}/upscale_models"
+    wget https://huggingface.co/vclansience/SD_lora/resolve/main/4x-UltraSharp.pth
+fi
+
+echo "Checking for '4xUltrasharp_4xUltrasharpV10.pt'"
+if [ ! -f "${ROOT}/upscale_models/4xUltrasharp_4xUltrasharpV10.pt" ]; then
+    echo "File '4xUltrasharp_4xUltrasharpV10.pt' was not found in the checkpoints. Downloading from 'https://huggingface.co/vclansience/SD_lora' "
+    cd "${ROOT}/upscale_models"
+    wget https://huggingface.co/vclansience/SD_lora/resolve/main/4xUltrasharp_4xUltrasharpV10.pt
 fi
 
 echo "End of script"
