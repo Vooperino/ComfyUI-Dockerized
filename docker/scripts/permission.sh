aa3d0e9e819e0151622e321eed4c4b7f9adc2fb0
Voop <viperlv2@inbox.lv>
2025-04-09 11:44:29 +0300

    Troubleshooting

--- docker/scripts/permission.sh
+++ docker/scripts/permission.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-PERMISSION_NUM=777
 TARGET_DIRS=(
   "/data/models"
   "/output"
 )
 
-echo "[Perm-fix] Permission updater started"
-
 while true; do
     for dir in "${TARGET_DIRS[@]}"; do
         if [ ! -z "$dir" ]; then
             if [ -d "$dir" ]; then
-                chmod -R $PERMISSION_NUM "$dir"
-                echo "[Perm-fix] Updated permissions on $dir"
+                chmod -R 777 "$dir"
             fi
         fi
     done
