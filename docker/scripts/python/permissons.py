5d7b868577a460e4bebf89d083cc5a768e6e597e
Voop <voop@voop.lv>
2025-07-11 12:19:42 +0300

    Loop Fix

--- docker/scripts/python/permissons.py
+++ docker/scripts/python/permissons.py
@@ -1,12 +1,15 @@
 from pathlib import Path
+import time
 
 TARGETS_PATHS = ["/data/models","/output"]
 
 if __name__ == '__main__':
-    for target_path in TARGETS_PATHS:
-        path = Path(target_path)
-        if path.exists():
-            print(f"[Permissions] Setting permissions for {target_path} to 777")
-            path.chmod(0o777)
-        else:
-            print(f"[Permissions] Path {target_path} does not exist, skipping permission change.")
+    while True:
+        for target_path in TARGETS_PATHS:
+            path = Path(target_path)
+            if path.exists():
+                print(f"[Permissions] Setting permissions for {target_path} to 777")
+                path.chmod(0o777)
+            else:
+                print(f"[Permissions] Path {target_path} does not exist, skipping permission change.")
+        time.sleep(10)  # Check every 10 seconds
