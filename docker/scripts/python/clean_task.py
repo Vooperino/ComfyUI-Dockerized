7eda46950f9fce52ffbc1509a05cccae66d939e9
Voop <voop@voop.lv>
2025-07-15 22:29:57 +0300

    Clean up

--- docker/scripts/python/clean_task.py
+++ docker/scripts/python/clean_task.py
@@ -29,53 +29,31 @@ def __startup_cleanup():
             elif item.is_dir():
                 shutil.rmtree(item)
 
-def __check_logging_retention(path):
+def __scan_output_directory(path):
     global CFG
-    if CFG.is_logging_retention_enabled():
-        log_amount = 0
-        for log_file in path.iterdir():
-            if log_file.is_dir():
-                __check_logging_retention(log_file)
-            elif log_file.is_file():
-                log_retention_age = CFG.get_logging_retention_age()
-                log_cutoff_time = datetime.now() - log_retention_age
-                if datetime.fromtimestamp(log_file.stat().st_mtime) < log_cutoff_time:
-                    LOGGER.log(f"(Logging Retention) Removing log file: {log_file}",True)
-                    log_file.unlink()
-                    log_amount += 1
-
-        if log_amount > 0:
-            LOGGER.log("Logging Retention) Removed {log_amount} log files older than {log_cutoff_time}.")
-            
-def __check_output_retention(path):
-    global CFG, LOGGER
-    if CFG.is_file_retention_enabled and not CFG.is_cleanup_directory_enabled():
-        output_amount = 0
-        for item in path.iterdir():
-            if item.is_dir():
-                __check_output_retention(item)
-            elif item.is_file():
-                retention_age = CFG.get_file_retention_age()
-                cutoff_time = datetime.now() - retention_age
-                print(f"{__LOG_PREFIX} (File Retention) Checking for files older than {cutoff_time}...")          
-                for item in OUTPUT_PATH.iterdir():
-                    if item.is_file() and datetime.fromtimestamp(item.stat().st_mtime) < cutoff_time:
-                        LOGGER.log(f"(File Retention) Removing file: {item}",True)
-                        item.unlink()
-                        output_amount += 1
-        if output_amount > 0:
-            LOGGER.log(f"(File Retention) Removed {output_amount} files older than {cutoff_time}.",True)
+    if not path.exists():
+        print(f"(File Retention) Output path {path} does not exist. Skipping scan.")
+        return
+    amount = 0
+    for outfile in path.iterdir():
+        if outfile.is_file():
+            retention_age = CFG.get_file_retention_age()
+            cutoff_time = datetime.now() - retention_age
+            if datetime.fromtimestamp(outfile.stat().st_mtime) < cutoff_time:
+                print(f"(File Retention) Removing file: {outfile}")
+                outfile.unlink()
+        elif outfile.is_dir():
+            __scan_output_directory(outfile)
+    if amount > 0:
+        print(f"(File Retention) Removed {amount} files older than {cutoff_time}.")
 
 if __name__ == '__main__':
     __load_config()
     __startup_cleanup()
     while True:
         __load_config() # Make the configuration to be dynamic
-        __check_logging_retention(LOG_PATH)
-        
         if CFG.is_file_retention_enabled and not CFG.is_cleanup_directory_enabled():
-            __check_output_retention(OUTPUT_PATH)
-
+            __scan_output_directory(OUTPUT_PATH)
         elif CFG.is_cleanup_directory_enabled() and not CFG.is_file_retention_enabled():
             cleanup_interval = CFG.get_cleanup_directory_interval()
             last_cleanup_time = datetime.now() - cleanup_interval
